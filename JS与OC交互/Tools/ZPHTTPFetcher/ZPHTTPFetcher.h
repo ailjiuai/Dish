bcb4cc5cebfad60b8b2662b64e5f76b9942e179c
iLogiEMAC <ilogiemac01@iLogiE-2.local>
2015-12-18 15:04:15 +0800

    HTTPFetcher 将请求数据封装

---
+++ JS与OC交互/Tools/ZPHTTPFetcher/ZPHTTPFetcher.h
@@ -0,0 +1,13 @@
+//
+//  ZPHTTPFetcher.h
+//  LearnDish
+//
+//  Created by iLogiEMAC on 15/12/18.
+//  Copyright © 2015年 zp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZPHTTPFetcher : NSObject
+
+@end
---
+++ JS与OC交互/Tools/ZPHTTPFetcher/ZPHTTPFetcher.m
@@ -0,0 +1,13 @@
+//
+//  ZPHTTPFetcher.m
+//  LearnDish
+//
+//  Created by iLogiEMAC on 15/12/18.
+//  Copyright © 2015年 zp. All rights reserved.
+//
+
+#import "ZPHTTPFetcher.h"
+
+@implementation ZPHTTPFetcher
+
+@end
